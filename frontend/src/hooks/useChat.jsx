ebd40f1f0c9cbaf9ddb4e3c697c099a2a87f8d52
AmanKadam-16 <aman16112002kadam@gmail.com>
2025-06-23 21:52:41 +0530

    baseurl change

--- frontend/src/hooks/useChat.jsx
+++ frontend/src/hooks/useChat.jsx
@@ -1,6 +1,6 @@
 import { createContext, useContext, useEffect, useState } from "react";
 
-const backendUrl = import.meta.env.VITE_API_URL || "http://localhost:3000";
+const backendUrl = import.meta.env.VITE_API_URL || "https://vksr7dqq-3000.inc1.devtunnels.ms";
 
 const ChatContext = createContext();
 
